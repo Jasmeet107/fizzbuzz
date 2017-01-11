bb84418697d665eee5edfafd66a6ebaf59bec224
Jasmeet Arora <jasmeet@mit.edu>
2017-01-10 23:19:23 -0500

    created answer function and unit tests

--- src/fizzbuzz_game.py
+++ src/fizzbuzz_game.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 
 sys.path.insert(0, os.getcwd())
@@ -11,4 +12,14 @@ def evaluate(count):
     if count%5 == 0:
         return 'buzz'
     return str(count)
+
+def answer(count, probability):
+    arbitor = (random.random() < probability)
+    if count%3 == 0 and count%5 == 0:
+        return 'fizzbuzz' if arbitor else random.choice([str(count), 'fizz', 'buzz'])
+    if count%3 == 0: 
+        return 'fizz' if arbitor else random.choice([str(count), 'buzz', 'fizzbuzz'])
+    if count%5 == 0:
+        return 'buzz' if arbitor else random.choice([str(count), 'fizz', 'fizzbuzz'])
+    return str(count) if arbitor else random.choice(['fizz', 'buzz', 'fizzbuzz'])
 
--- tests/test_fizzbuzz_game.py
+++ tests/test_fizzbuzz_game.py
@@ -1,4 +1,6 @@
 import unittest
+import mock
+from mock import patch
 from src import fizzbuzz_game
 
 class TestFizzBuzzGame(unittest.TestCase): 
@@ -13,6 +15,7 @@ class TestFizzBuzzGame(unittest.TestCase):
         """
         Tests that a valid integer is properly evaluated based on the count
         """
+        #do I want to be hardcoding these values? 
         self.assertEqual(fizzbuzz_game.evaluate(1), '1')
         
     def test_evaluates_correct_fizz(self):
@@ -33,6 +36,69 @@ class TestFizzBuzzGame(unittest.TestCase):
         """
         self.assertEqual(fizzbuzz_game.evaluate(15), 'fizzbuzz')
 
+    @mock.patch('random.random', return_value=0)
+    def test_returns_correct_number_when_arbitor_returns_True(self, getrandbits_function):
+        """
+        Tests that the count number is returned if the arbitor returns true and it is not mod 3 or 5 
+        """
+        self.assertEqual(fizzbuzz_game.answer(1, 0.5), '1')
+        assert getrandbits_function.called
+
+    @mock.patch('random.random', return_value=0)
+    def test_returns_correct_fizz_when_arbitor_returns_True(self, getrandbits_function):
+        """
+        Tests that "fizz" is returned if the arbitor returns true and the count is mod 3  
+        """
+        self.assertEqual(fizzbuzz_game.answer(3, 0.5), 'fizz')
+        assert getrandbits_function.called
+
+    @mock.patch('random.random', return_value=0)
+    def test_returns_correct_buzz_when_arbitor_returns_True(self, getrandbits_function):
+        """
+        Tests that "buzz" is returned if the arbitor returns true and the count is mod 5  
+        """
+        self.assertEqual(fizzbuzz_game.answer(5, 0.5), 'buzz')
+        assert getrandbits_function.called
+
+    @mock.patch('random.random', return_value=0)
+    def test_returns_correct_fizzbuzz_when_arbitor_returns_True(self, getrandbits_function):
+        """
+        Tests that "fizzbuzz" is returned if the arbitor returns true and the count is mod 3 and 5  
+        """
+        self.assertEqual(fizzbuzz_game.answer(15, 0.5), 'fizzbuzz')
+        assert getrandbits_function.called
+
+    @mock.patch('random.random', return_value=1)
+    def test_incorrectly_returns_fizz_buzz_or_fizzbuzz_when_arbitor_returns_False(self, getrandbits_function):
+        """
+        Tests that "fizz", "buzz", or "fizzbuzz" is returned if the arbitor returns true and it is not mod 3 or 5 
+        """
+        self.assertIn(fizzbuzz_game.answer(1, 0.5), ['fizz', 'buzz', 'fizzbuzz'])
+        assert getrandbits_function.called
+
+    @mock.patch('random.random', return_value=1)
+    def test_incorrectly_returns_count_buzz_or_fizzbuzz_when_arbitor_returns_False(self, getrandbits_function):
+        """
+        Tests that the count, "buzz", or "fizzbuzz" is returned if the arbitor returns true and the count is mod 3  
+        """
+        self.assertIn(fizzbuzz_game.answer(3, 0.5), ['1', 'buzz', 'fizzbuzz'])
+        assert getrandbits_function.called
+
+    @mock.patch('random.random', return_value=1)
+    def test_incorrectly_returns_count_fizz_or_fizzbuzz_when_arbitor_returns_False(self, getrandbits_function):
+        """
+        Tests that the count, "fizz", or "fizzbuzz" is returned if the arbitor returns true and the count is mod 5  
+        """
+        self.assertIn(fizzbuzz_game.answer(5, 0.5), ['fizz', '5', 'fizzbuzz'])
+        assert getrandbits_function.called
+
+    @mock.patch('random.random', return_value=1)
+    def test_incorrectly_returns_count_fizz_or_buzz_when_arbitor_returns_False(self, getrandbits_function):
+        """
+        Tests that the count, "fizz", or "buzz" is returned if the arbitor returns true and the count is mod 3 and 5  
+        """
+        self.assertIn(fizzbuzz_game.answer(15, 0.5), ['fizz', 'buzz', '15'])
+        assert getrandbits_function.called
     
 if __name__ == '__main__': 
     unittest.main()
